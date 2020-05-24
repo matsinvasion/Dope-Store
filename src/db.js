07e4975acddf227d28ab3de52785e57899bd0aac
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 12:47:32 +0300

    revise declaration

--- src/db.js
+++ src/db.js
@@ -2,9 +2,9 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-    typeDefs:"../generated/prisma.graphql",
-    enpoint:"process.env.PRISMA_ENDPOINT",
-    secret:"process.env.PRISMA_SECRET",
+    typeDefs:"./generated/prisma.graphql",
+    endpoint:process.env.PRISMA_ENDPOINT,
+    secret:process.env.PRISMA_SECRET,
     debug:false
 });
 
