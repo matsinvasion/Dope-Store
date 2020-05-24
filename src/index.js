4a73c502126290a300d39448608533b935b46666
matsinvasion <matkmusasizi@gmail.com>
2020-05-24 12:48:16 +0300

    space

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ require('dotenv').config({path: "variables.env"});
 const createServer = require("./createServer");
 const db = require("./db");
 
+const server = createServer();
+
+
+server.start({
+    cors:{
+        credentials:true,
+        origin: process.env.FRONTEND_URL, 
+    }
+    
+}, details => {console.log(`Server is running on on port http:/localhost:${details.port}`)});
