95081d42a3c44820d171b44010691d4dbf146fd9
matsinvasion <matkmusasizi@gmail.com>
2020-05-26 20:56:04 +0300

    resolvers for mutations

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,28 @@
-const Mutation = {};
+const Mutations = {
+    async createItem(parent,args,ctx,info){
+        const item = await ctx.db.mutation.createItem({
+            data:{
+                ...args
+            }
+        },info)
+        return item;
+    },
+    updateItem(parent,args,ctx,info){
+        //first take copy of the updates
+        const updates= { ...args };
+        //remove id from updates, since an id is never updated
+        delete updates.id;
+        //run update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where:{
+                id:args.id
+            }
+        }, info
+        )
 
-module.exports=Mutation;
+    }
+   
+};
+
+module.exports=Mutations;
